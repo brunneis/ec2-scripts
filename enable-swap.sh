de2cce5ea663c0757342e90b58a1a67edd8848d3
Rodrigo Martínez <dev@brunneis.com>
2018-03-24 20:43:52 +0100

    Update enable-swap.sh

--- enable-swap.sh
+++ enable-swap.sh
@@ -4,4 +4,4 @@ sudo dd if=/dev/zero of=/var/swap bs=1M count=4096
 sudo mkswap /var/swap
 sudo chmod 0600 /var/swap
 sudo swapon /var/swap
-sudo echo '/var/swap swap swap defaults 0 0' > /etc/fstab
+sudo su -c 'echo "/var/swap swap swap defaults 0 0" >> /etc/fstab'
